717f7ba4e7522b23371ddbc691771fe892455484
shizhe <shizhe_zhang@outlook.com>
2023-10-07 19:09:35 -0700

    refactor fundMe in deploy and test

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -7,10 +7,11 @@ import {FundMe} from "../src/FundMe.sol";
 
 
 contract DeployFundMe is Script{
-    function run() external {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        new FundMe();
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
 
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
         // us -> FundMeTest -> FundMe
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
@@ -18,7 +20,7 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
 
     }
 
